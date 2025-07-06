909a8f3f638e9aecded26d7adb8a86a40d825441
Synogun <neto.antoniorrvv@gmail.com>
2025-07-05 23:36:04 -0300

    1.0.0

---
+++ dist/FarmRPGPlus.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name FarmRPG Plus
+// @description Add features to Farm RPG while keeping code of conduct in mind.
+// @version 1.0.0
+// @author Synogun
+// @match https://farmrpg.com/*
+// @grant none
+// @icon https://farmrpg.com/favicon.ico
+// @license MIT
+// @namespace Violentmonkey Scripts
+// ==/UserScript==
