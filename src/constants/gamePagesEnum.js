eca96b06b8e2d90ba292b1a8454ff9e0624247f5
Synogun <neto.antoniorrvv@gmail.com>
2025-07-08 20:54:07 -0300

    chore: add more links for pages and icons

--- src/constants/gamePagesEnum.js
+++ src/constants/gamePagesEnum.js
@@ -39,6 +39,7 @@ const TownPageLinks = Object.freeze({
     BORGEN_MERCANTILE: 'bmerc',
     SPIN: 'spin',
     BANK: 'bank',
+    VAULT: 'crack',
     STEAK_MARKET: 'steakmarket',
     TEMPLE: 'temple',
     FISHING_CHARTER: 'charter',
@@ -58,6 +59,70 @@ const TownPageLinks = Object.freeze({
     TOWN_HALL: 'townhall',
 });
 
+const ExplorePageLinks = Object.freeze({
+    EXPLORE: 'explore',
+    FOREST: 'area.php?id=7',
+    SMALL_CAVE: 'area.php?id=1',
+    SMALL_SPRING: 'area.php?id=2',
+    HIGHLAND_HILLS: 'area.php?id=3',
+    CANE_POLE_RIDGE: 'area.php?id=4',
+    MISTY_FOREST: 'area.php?id=5',
+    BLACK_ROCK_CANYON: 'area.php?id=6',
+    MOUNT_BANON: 'area.php?id=8',
+    EMBER_LAGOON: 'area.php?id=9',
+    WHISPERING_CREEK: 'area.php?id=10',
+    JUNDLAND_DESERT: 'area.php?id=13',
+    GARY_CRUSHROOM: 'area.php?id=20',
+    EXPEDITION: 'expedition.php',
+});
+
+const ExploreInfoPageLinks = Object.freeze({
+    FOREST_INFO: 'location.php?type=explore&id=7',
+    SMALL_CAVE_INFO: 'location.php?type=explore&id=1',
+    SMALL_SPRING_INFO: 'location.php?type=explore&id=2',
+    HIGHLAND_HILLS_INFO: 'location.php?type=explore&id=3',
+    CANE_POLE_RIDGE_INFO: 'location.php?type=explore&id=4',
+    MISTY_FOREST_INFO: 'location.php?type=explore&id=5',
+    BLACK_ROCK_CANYON_INFO: 'location.php?type=explore&id=6',
+    MOUNT_BANON_INFO: 'location.php?type=explore&id=8',
+    EMBER_LAGOON_INFO: 'location.php?type=explore&id=9',
+    WHISPERING_CREEK_INFO: 'location.php?type=explore&id=10',
+    JUNDLAND_DESERT_INFO: 'location.php?type=explore&id=13',
+    GARY_CRUSHROOM_INFO: 'location.php?type=explore&id=20',
+});
+
+const FishingPageLinks = Object.freeze({
+    FISHING: 'fish',
+    FARM_POND: 'fishing.php?id=2',
+    SMALL_POND: 'fishing.php?id=1',
+    FOREST_POND: 'fishing.php?id=3',
+    LAKE_TEMPEST: 'fishing.php?id=4',
+    SMALL_ISLAND: 'fishing.php?id=5',
+    CRYSTAL_RIVER: 'fishing.php?id=6',
+    EMERALD_BEACH: 'fishing.php?id=7',
+    VAST_OCEAN: 'fishing.php?id=8',
+    LAKE_MINERVA: 'fishing.php?id=9',
+    LARGE_ISLAND: 'fishing.php?id=10',
+    PIRATES_COVE: 'fishing.php?id=11',
+    GLACIER_LAKE: 'fishing.php?id=12',
+    CHARTER: 'charter.php'
+});
+
+const FishingInfoPageLinks = Object.freeze({
+    FARM_POND_INFO: 'location.php?type=fishing&id=2',
+    SMALL_POND_INFO: 'location.php?type=fishing&id=1',
+    FOREST_POND_INFO: 'location.php?type=fishing&id=3',
+    LAKE_TEMPEST_INFO: 'location.php?type=fishing&id=4',
+    SMALL_ISLAND_INFO: 'location.php?type=fishing&id=5',
+    CRYSTAL_RIVER_INFO: 'location.php?type=fishing&id=6',
+    EMERALD_BEACH_INFO: 'location.php?type=fishing&id=7',
+    VAST_OCEAN_INFO: 'location.php?type=fishing&id=8',
+    LAKE_MINERVA_INFO: 'location.php?type=fishing&id=9',
+    LARGE_ISLAND_INFO: 'location.php?type=fishing&id=10',
+    PIRATES_COVE_INFO: 'location.php?type=fishing&id=11',
+    GLACIER_LAKE_INFO: 'location.php?type=fishing&id=12',
+});
+
 const EventPageLinks = Object.freeze({
     BOTTLE_ROCKET: 'Bottle-Rocket-Brawl'
 });
@@ -73,8 +138,19 @@ const GamePagesEnum = Object.freeze({
     ...NavBarLinks,
     ...HomePageLinks,
     ...TownPageLinks,
+    ...ExplorePageLinks,
+    ...ExploreInfoPageLinks,
+    ...FishingPageLinks,
+    ...FishingInfoPageLinks,
     ...EventPageLinks,
     ...SinglePageLinks,
 });
 
 export default GamePagesEnum;
+
+export {
+    EventPageLinks, ExploreInfoPageLinks, ExplorePageLinks,
+    FishingInfoPageLinks, FishingPageLinks, HomePageLinks,
+    NavBarLinks, SinglePageLinks, TownPageLinks
+};
+
